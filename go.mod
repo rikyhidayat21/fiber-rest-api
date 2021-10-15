09a6429a759bb0ecfdd7981617e37617c67cf16b
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-10-15 11:26:29 +0700

    connect with fiber, hello world

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rikyhidayat21/fiber-rest-api
 
 go 1.15
+
+require github.com/gofiber/fiber/v2 v2.20.2 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
-	fmt.Println("initial fiber")
+	// inisialisasi fiber
+	app := fiber.New() 
+
+	// middleware logger
+	app.Use(logger.New())
+
+	// Get route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"Hello": "world",
+		})
+	})
+
+	app.Listen(":3000")
 }
