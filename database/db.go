30c34e8781a7dde4148f247e9dd331f3eb21d0c6
hdytxr <rikyhidayat21@student.upi.edu>
2021-10-15 16:24:47 +0700

    config db

--- database/db.go
+++ database/db.go
@@ -3,9 +3,11 @@ package database
 import (
 	"fmt"
 	"log"
+	"strconv"
 
+	"github.com/rikyhidayat21/fiber-rest-api/config"
 	"github.com/rikyhidayat21/fiber-rest-api/model"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -15,20 +17,18 @@ var DB *gorm.DB
 // ConnectDB for connect the DB
 func ConnectDB() {
 	var err error
-	// p := config.Config("DB_PORT")
-	// port, err := strconv.ParseUint(p, 10, 32)
+	p := config.Config("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
 
 	if err != nil {
 		log.Println("Error to connect mysql")
 	}
 
 	// Connection URL to connect to Mysql Database
-	dsn := "root:@tcp(127.0.0.1:3306)/sebat-go?charset=utf8mb4&parseTime=True&loc=Local"
-
-	// dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%s)/%s?charset=utf8mb4&parseTime=True&loc-Local",config.Config("DB_USER"),config.Config("DB_PASSWORD"), port, config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
 
 	// Connect to the DB and initialize the DB variable
-	DB, err = gorm.Open(mysql.Open(dsn))
+	DB, err = gorm.Open(postgres.Open(dsn))
 
 	if err != nil {
 		panic("Failed to connect to database")
--- go.mod
+++ go.mod
@@ -3,10 +3,18 @@ module github.com/rikyhidayat21/fiber-rest-api
 go 1.15
 
 require (
+	github.com/alexbrainman/sspi v0.0.0-20180613141037-e580b900e9f5 // indirect
 	github.com/cespare/reflex v0.3.1 // indirect
 	github.com/gofiber/fiber/v2 v2.20.2
 	github.com/google/uuid v1.3.0
+	github.com/jcmturner/gokrb5/v8 v8.2.0 // indirect
 	github.com/joho/godotenv v1.4.0
+	gopkg.in/jcmturner/aescts.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/dnsutils.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
+	gopkg.in/jcmturner/gokrb5.v7 v7.5.0 // indirect
+	gopkg.in/jcmturner/rpc.v1 v1.1.0 // indirect
 	gorm.io/driver/mysql v1.1.2
+	gorm.io/driver/postgres v1.1.2
 	gorm.io/gorm v1.21.16
 )
